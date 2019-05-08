29897b8b228d65d455762fce6188cc00577bdb4e
Balor <balor@programmer.net>
2019-05-08 14:21:49 +0530

    Search Bar

---
+++ app/static/js/script.js
@@ -0,0 +1,20 @@
+var dict = {
+    'google': 'https://google.com/search?q=QUERY',
+    'duckduckgo': 'https://duckduckgo.com/q=QUERY',
+    'google_images': 'https://www.google.com/search?tbm=isch&q=QUERY'
+};
+
+function search_query () 
+{
+    var engine = document.getElementById ('search_engine').value;
+    var query = document.getElementById ('query').value;
+    if ( ! (query.startsWith ('\"') || query.startsWith ("'"))) {
+        query = query.trim();
+        query = query.replace(/\s+/g, '+')
+    }
+    console.log  ("Your Query: " + query);
+    var search_url = dict[engine].replace ("QUERY", query);
+    console.log ("Search URL: " + search_url);
+
+    window.open (search_url, "_self");
+}
